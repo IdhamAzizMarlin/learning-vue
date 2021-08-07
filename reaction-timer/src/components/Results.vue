09e34a998f1eaa591edb24e9d95083897a57eace
iamsomnia <idhamaziz390@gmail.com>
2021-08-07 16:14:27 +0700

    Game With Vue

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+  <p><span class="text-bold">Reaction Time : </span> {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail Pace...";
+    }
+  },
+};
+</script>
+
+<style>
+.text-bold {
+  font-weight: bold;
+}
+
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
